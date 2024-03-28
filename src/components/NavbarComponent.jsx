57ec4eabc1c946a927c10a73131ae8acc9bb1a2b
Darshhhhh <contact.shahdarsh@gmail.com>
2024-03-28 16:30:18 -0400

    Navbar Fix

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -3,26 +3,31 @@ import { useNavigate, Link } from "react-router-dom";
 export default function NavbarComponent(props) {
   const navigate = useNavigate();
   return (
-    <div className="navbar">
-      <nav className="leftNavbarContainer">
-        <div className="navItem" onClick={() => navigate("/")}>
-          Home
+    <div className="navbar flex justify-between">
+        <nav className="leftNavbarContainer">
+          <div className="centerNavbarContainer">Chain4Scholars</div>
+        </nav>
+        <div className="rightNavbarContainer">
+          <div className="navItem" onClick={() => navigate("/")}>
+            Home
+          </div>
+          <div className="navItem" onClick={() => navigate("discover")}>
+            Discover
+          </div>
+          <div className="navItem" onClick={() => navigate("create_project")}>
+            Start a project
+          </div>
+          <div className="navItem">
+            <Link to="/profile" state={{ address: props.address }}>
+              {props.address.slice(0, 5) +
+                "..." +
+                props.address.slice(
+                  props.address.length - 4,
+                  props.address.length
+                )}
+            </Link>
+          </div>
         </div>
-        <div className="navItem" onClick={() => navigate("discover")}>
-          Discover
-        </div>
-        <div className="navItem" onClick={() => navigate("create_project")}>
-          Start a project
-        </div>
-      </nav>
-      <div className="centerNavbarContainer">Chain4Scholars</div>
-      <div className="rightNavbarContainer">
-        <div className="navItem">
-          <Link to="/profile" state={{ address: props.address }}>
-            {props.address.slice(0,5) + "..." + props.address.slice(props.address.length - 4, props.address.length)}
-          </Link>
-        </div>
-      </div>
     </div>
   );
 }
