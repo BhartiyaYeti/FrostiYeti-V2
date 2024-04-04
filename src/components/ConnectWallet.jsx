1226e07181f8adc57a8d68e06baee8d95891125c
Darshhhhh <contact.shahdarsh@gmail.com>
2024-04-04 01:52:07 -0400

    Ui Fixes

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -4,40 +4,18 @@ import SubNav from "./SubNav";
 export default function ConnectWallet(props) {
   return (
     <>
-      {/* <div className="p-5 px-10">
-        <div className="flex justify-between">
-          <div></div>
-          <hr class="text-end w-52 h-[0.1px] my-4 bg-gray-100 border-0 roundeddark:bg-white"></hr>
-        </div>
-        <div className="flex my-52 justify-center gap-14">
-          <div>
-            <img src={HeroLogo} alt="" className="w-[300px] " />
-          </div>
-          <div className="my-auto w-[600px]">
-            <h1 className="text-5xl font-bold mb-3 typing">
-              EduLink
-            </h1>
-            <span className="text-white">
-              Experience the power of blockchain in student financing:
-              Effortlessly fund your education, shaping tomorrow's leaders
-              today...
-            </span>
-            <div>
-              <button
-                className="walletButton mt-10"
-                onClick={props.connectMetamask}
-              >
-                Connect to Metamask //{" "}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
-
       <div>
         <SubNav connectMetamask={props} />
         <div className="px-44 h-[80vh] flex items-center justify-between">
-          <div>
+          <div
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 1, ease: "linear" }}
+            variants={{
+              visible: { opacity: 1 },
+              hidden: { opacity: 0 },
+            }}
+          >
             <h1 className="text-white text-6xl w-[750px] font-extrabold">
               <span className="inline-block mb-5">Now</span>
               <br></br>
